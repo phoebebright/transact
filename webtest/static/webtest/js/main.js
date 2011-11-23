b4f0668c25aa86da0a74bc0fd665550818842389
Grzegorz Borys <engrost@gmail.com>
2011-11-23 00:01:03 +0000

    webtest: first call done need csrf to add to ajax request...

--- webtest/static/webtest/js/main.js
+++ webtest/static/webtest/js/main.js
@@ -1,1 +1,12 @@
 /* js file for webtest app */
+function display_call(data, textStatus, jqXHR){
+    jQuery('#call_response').text(data);
+}
+function send_call() {
+   var data = jQuery('#call_area').val();
+   jQuery.post('/api/', data, display_call, 'json')
+}
+
+jQuery(document).ready(function() {
+   jQuery('#call_send').click(send_call);
+ });
