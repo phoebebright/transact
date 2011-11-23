f2e1b34aa7b3c97fc7b6f3939c59339f396eeac5
Grzegorz Borys <engrost@gmail.com>
2011-11-23 09:30:04 +0000

    webtest: response still does not work.

--- webtest/static/webtest/js/main.js
+++ webtest/static/webtest/js/main.js
@@ -1,10 +1,11 @@
 /* js file for webtest app */
 function display_call(data, textStatus, jqXHR){
+    alert(data);
     jQuery('#call_response').text(data);
 }
 function send_call() {
    var data = jQuery('#call_area').val();
-   jQuery.post('/api/', data, display_call, 'json')
+   jQuery.post('/api/', data, function(a,b,c){display_call(a,b,c)}, 'json');
 }
 
 jQuery(document).ready(function() {
