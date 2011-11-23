90894813953fd7015067f7f6510d35b2c40ba3ed
Lukasz Baczynski <contact@lukaszbaczynski.com>
2011-11-23 17:02:08 +0000

    JQuery ui instead of stardard alert box

--- webtest/urls.py
+++ webtest/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'webtest.views.test_view'),
     url(r'^PRICECHECK/$', 'webtest.views.price_check'),
+    url(r'^TRANSACT/$', 'webtest.views.transact'),
 )
