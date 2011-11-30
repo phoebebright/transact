d79af95d87944ebf06f0232daa2f5e26815820f8
Lukasz Baczynski <contact@lukaszbaczynski.com>
2011-11-30 11:12:02 +0000

    Flight demo

--- webtest/urls.py
+++ webtest/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'webtest.views.test_view'),
+    url(r'^flight_demo/$', 'webtest.views.flight_demo'),
     url(r'^PRICECHECK/$', 'webtest.views.price_check'),
     url(r'^TRANSACT/$', 'webtest.views.transact'),
 )
