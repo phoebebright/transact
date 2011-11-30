7a7d62723085e2935f8169d88fecdfb9e34bad74
Phoebe B <phoebebright@spamcop.net>
2011-11-30 12:51:20 +0000

    pricecheck page now has auth

--- webtest/urls.py
+++ webtest/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'webtest.views.test_view'),
     url(r'^flight_demo/$', 'webtest.views.flight_demo'),
-    url(r'^PRICECHECK/$', 'webtest.views.price_check'),
-    url(r'^TRANSACT/$', 'webtest.views.transact'),
+
 )
