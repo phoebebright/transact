71c07a50b238c4d95062f0e79a8a763de9385bea
Grzegorz Borys <engrost@gmail.com>
2011-12-11 20:00:12 +0000

    use of MyDriver features

--- webtest/seltests.py
+++ webtest/seltests.py
@@ -1,6 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
 from django_selenium.testcases import SeleniumTestCase
 
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 
@@ -11,18 +11,12 @@ class BaseSeleniumTextCase(SeleniumTestCase):
         self.auth_url = "http://testuser:silicon@transactcarbon.com/"
         self.base_url = "http://transactcarbon.com"
         self.driver.get(self.auth_url)
-
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-
-    def is_text_present (self, string):
-        if str(string) in self.driver.page_source:
-            return True
-        else:
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e:
             return False
-
+        return True
     def get_relative(self, url):
         self.driver.get(self.base_url + url)
 
@@ -36,24 +30,22 @@ class Examples(BaseSeleniumTextCase):
         driver.find_element_by_id("id_quantity").clear()
         driver.find_element_by_id("id_quantity").send_keys("4")
         driver.find_element_by_id("call_api").click()
-        self.is_text_present("total in EUR")
+        driver.is_text_present("total in EUR")
         driver.find_element_by_xpath("//button[@type='button']").click()
-        self.is_text_present('"call":"PRICECHECK",')
+        driver.is_text_present('"call":"PRICECHECK",')
 
     def test_purchase_flight(self):
         driver = self.driver
         self.get_relative('/')
         driver.find_element_by_link_text("Examples").click()
-        self.is_text_present('Purchase Flight (buy by weight)')
+        driver.is_text_present('Purchase Flight (buy by weight)')
         driver.find_element_by_link_text("Purchase Flight (buy by weight)").click()
         for i in range(60):
-            try:
-                if self.is_element_present(By.XPATH, "//tr[td>0]/td[@id=\"carbon\"]"):
-                    break
-            except: pass
+            if self.is_element_present(By.XPATH, "//tr[td>0]/td[@id=\"carbon\"]"):
+                break
             time.sleep(1)
         else:
             self.fail("time out")
         driver.find_element_by_xpath("//span[@id='approvePay']").click()
-        self.is_text_present("Successfully paid")
+        driver.is_text_present("Successfully paid")
         driver.find_element_by_xpath("//button[@type='button']").click()
