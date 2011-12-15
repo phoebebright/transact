925264b0cfeca12c05cd86c333c807ae6222eac6
Grzegorz Borys <engrost@gmail.com>
2011-12-15 12:01:31 +0000

    overriding of TransactAPI.gate now only when settings.DEBUG=True

--- webtest/views.py
+++ webtest/views.py
@@ -1,6 +1,7 @@
 #python
 
 #libs
+from django.conf import settings
 from django.shortcuts import render_to_response
 
 #local
@@ -17,21 +18,21 @@ def price_check(request):
 
     form = webtest_forms.PriceCheck(initial={"call":"PRICECHECK", "token":"dummy_token", "testmode":'false'})
 
-    return render_to_response("pricecheck.html", {"form": form})
+    return render_to_response("pricecheck.html", {"form": form, "debug_setting": settings.DEBUG})
 
 
 def transact(request):
     # LUKASZ - see transact version in web/views that doesn't use APIs 
     form = webtest_forms.TransAct(initial={"call":"TRANSACT", "token":"dummy_token", "testmode":"false"})
 
-    return render_to_response("transact.html", {"form": form})
+    return render_to_response("transact.html", {"form": form, "debug_setting": settings.DEBUG})
 
 
 def flight_demo(request):
 
-    return render_to_response("flight_demo.html")
+    return render_to_response("flight_demo.html", {"form": form, "debug_setting": settings.DEBUG})
 
 
 def voucher_demo(request):
 
-    return render_to_response("voucher_demo.html")
+    return render_to_response("voucher_demo.html", {"form": form, "debug_setting": settings.DEBUG})
