899ad8cdd196bfd0729b021af542fbb4e3d1df5e
Grzegorz Borys <engrost@gmail.com>
2012-03-15 14:57:00 +0000

    mail result to multiple developers

--- deployment/runtests.sh
+++ deployment/runtests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SUBJECT="Test results `date`"
-TO="engrost@gmail.com"
+TO="engrost@gmail.com,phoebebright@vividlogic.ie"
 MESSAGE="report.txt"
 
 #run tests
 echo "Start time: `date`" >> $MESSAGE
 #./manage.py test --selenium >> $MESSAGE 2>&1
-./manage.py test >> $MESSAGE 2>&1
+./manage.py test api web webtest  --noinput >> $MESSAGE 2>&1
 
 #email report
 mail -s "$SUBJECT" "$TO" < $MESSAGE
